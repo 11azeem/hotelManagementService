0d248226bf9736ea5e0868575528ea9cc351dd61
11azeem <11azeemuddin@gmail.com>
2024-09-27 23:38:25 +0530

    Update MainRestController.java

--- src/main/java/com/hms/hotelmanagementservice/controller/MainRestController.java
+++ src/main/java/com/hms/hotelmanagementservice/controller/MainRestController.java
@@ -1,13 +1,15 @@
 package com.hms.hotelmanagementservice.controller;
 
+import com.hms.hotelmanagementservice.Room;
 import com.hms.hotelmanagementservice.RoomRepository;
 import com.hms.hotelmanagementservice.model.GetAllRoomsResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Tag(name = "Hotel Management Service", description = "Hotel Management Service APIs")
 @RestController
@@ -28,5 +30,14 @@ public class MainRestController {
         getAllRoomsResponse.setRooms(roomRepository.findAll());
 
         return ResponseEntity.ok(getAllRoomsResponse);
+    }
+
+    @PostMapping("/getRoomAvailability")
+    public ResponseEntity<Boolean> getRoomAvailability(@RequestBody Integer roomId) {
+
+        Optional<Room> room = roomRepository.findById(roomId);
+
+        return room.map(value -> ResponseEntity.ok(value.getAvailable()))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
